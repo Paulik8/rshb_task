c16bf92c4a031be3165c21c74264c63cbae483f4
Paulik8 <ikhotin@mail.ru>
2020-08-23 13:17:03 +0300

    readme

--- lib/imageLoader.dart
+++ lib/imageLoader.dart
@@ -1,16 +1,9 @@
 import 'dart:async';
-import 'dart:io';
-import 'dart:ui' as ui;
-import 'package:fish_redux/fish_redux.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:path_provider/path_provider.dart';
-import 'package:rshb_test/home/action.dart';
 import 'package:rshb_test/product/models/cachedImage.dart';
-
-final String url = '/Users/paulik/Projects/flutterApps/rshb_test';
 
 Future<CachedImage> getImageFileFromAssets(String path) async {
   final byteData = await rootBundle.load('$path');
